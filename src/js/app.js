0af62101ff4a667790cf43b08ead4728386fb98f
JasurAvazov <jasuravazov4@gmail.com>
2023-01-31 17:27:00 +0500

    init

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as functions from "./modules/functions.js";
+import * as swipers from "./modules/swipers.js";
+import * as header from "./modules/header.js";
+
+functions.isWebp();
+header.init();
+swipers.init();
