a06c3a55951d3b3020e9373a20fc711530a838e6
JasurAvazov <jasuravazov4@gmail.com>
2023-02-01 10:54:19 +0500

    intro

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,3 +1,17 @@
 export function init() {
-    
+
+    // body paddingTop
+    const header = document.querySelector('.header').offsetHeight + 25
+    document.querySelector('.intro').style.marginTop = header+'px'
+
+    // header btns
+    const headerBtns = document.querySelectorAll('.header-btn')
+    headerBtns.forEach(el => {
+        el.addEventListener('click',() => {
+            headerBtns.forEach(all => {
+                all.classList.remove('active')
+            })
+            el.classList.add('active')
+        })
+    })
 }
