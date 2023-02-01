fb925c213f9972d0cf483078ef9da3d16dbd6650
JasurAvazov <jasuravazov4@gmail.com>
2023-02-01 11:21:07 +0500

    features block

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
 import * as functions from "./modules/functions.js";
 import * as swipers from "./modules/swipers.js";
 import * as header from "./modules/header.js";
+import SmoothScroll from "smoothscroll-for-websites";
+
+SmoothScroll({
+    animationTime: 1000,
+    stepSize: 60,
+    keyboardSupport: true,
+    arrowScroll: 100,
+    touchpadSupport: true,
+  });
 
 functions.isWebp();
 header.init();
