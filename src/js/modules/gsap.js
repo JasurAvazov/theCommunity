ec570374f8e2039305d48bcd4c123cd7cbc09780
JasurAvazov <jasuravazov4@gmail.com>
2023-02-02 11:59:42 +0500

    init gsap and fix

--- src/js/modules/gsap.js
+++ src/js/modules/gsap.js
@@ -1,3 +1,28 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger.js";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export function init(){
-    
+    gsap.utils.toArray(".places-top").forEach((section) => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: section,
+                start: "top 80%",
+                end: "top 20%",
+                scrub: 2,
+                markers: false,
+            },
+        });
+        tl.add("start")
+            .from(
+                section.querySelector(".box-left-side"),
+                {
+                x: -200,
+                opacity: 0,
+                ease: "expo.ease",
+                },
+            "start"
+          )
+    });
 }
